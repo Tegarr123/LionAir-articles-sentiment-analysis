c81cb6dcc73e36b39d9070501e0cd37221f5e563
Tegarr123 <fadhino987@gmail.com>
2025-12-13 18:35:16 +0700

    Update articles

--- scrapping_script/main.py
+++ scrapping_script/main.py
@@ -3,7 +3,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 get_links = pd.read_csv("https://docs.google.com/spreadsheets/d/1nE_v4AFkLM76TdWcAbU1HRWgQ-gGKYa9ZMBbm7MM0V0/export?gid=0&format=csv")
-get_links = get_links[['Kategori', 'Link']]
+get_links = get_links[['Kategori', 'Link', 'Label Sentimen']]
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
